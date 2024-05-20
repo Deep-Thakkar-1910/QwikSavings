f46ba0fcf8c9ae4dc6d571b56ae1921d450c9180
Deep-Thakkar-1910 <dthakkar477@gmail.com>
2024-05-20 13:18:45 +0530

    Initial Commit

---
+++ app/api/auth/[...nextauth]/route.ts
@@ -0,0 +1,24 @@
+// import { AuthOptions } from "next-auth";
+// import CredentialsProvider from "next-auth/providers/credentials";
+// const authOptions: AuthOptions = {
+//   providers: [
+//     CredentialsProvider({
+//       name: "Credentials",
+//       credentials: {
+//         email: {
+//           label: "Email",
+//           type: "email",
+//           placeholder: "Johndoe123@gmail.com",
+//         },
+//         passsword: {
+//           label: "Password",
+//           type: "password",
+//           placeholder: "Enter Your Password",
+//         },
+//       },
+//       async authorize(credentials) {
+
+//       },
+//     }),
+//   ],
+// };
