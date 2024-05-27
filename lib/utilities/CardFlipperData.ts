87ff65118f5df137f5de7459ec5bb5b43d57fa38
Deep-Thakkar-1910 <dthakkar477@gmail.com>
2024-05-27 12:31:34 +0530

    Added dummy images for demo purposes

--- lib/utilities/CardFlipperData.ts
+++ lib/utilities/CardFlipperData.ts
@@ -10,29 +10,36 @@ export const CardFlipperData: CardData[] = [
   {
     id: "1",
     name: "Card 1",
-    logoUrl: "https://via.placeholder.com/150",
+    logoUrl:
+      "https://media.istockphoto.com/id/95442265/photo/lottery.jpg?s=2048x2048&w=is&k=20&c=ITM_3NwSm8S7D-XT1h-ak_Sah7jI0_9DxcTvweS84gk=",
     thumbnailUrl: "https://via.placeholder.com/600X400",
     content: "This is card 1",
   },
   {
     id: "2",
     name: "Card 2",
-    logoUrl: "https://via.placeholder.com/150",
-    thumbnailUrl: "https://via.placeholder.com/600X400",
+    logoUrl:
+      "https://media.istockphoto.com/id/95442265/photo/lottery.jpg?s=2048x2048&w=is&k=20&c=ITM_3NwSm8S7D-XT1h-ak_Sah7jI0_9DxcTvweS84gk=",
+    thumbnailUrl:
+      "https://media.istockphoto.com/id/95442265/photo/lottery.jpg?s=2048x2048&w=is&k=20&c=ITM_3NwSm8S7D-XT1h-ak_Sah7jI0_9DxcTvweS84gk=",
     content: "This is card 2",
   },
   {
     id: "3",
     name: "Card 3",
-    logoUrl: "https://via.placeholder.com/150",
-    thumbnailUrl: "https://via.placeholder.com/600X400",
+    logoUrl:
+      "https://media.istockphoto.com/id/1724789360/photo/digital-security-concept-and-cloud-computing-security.jpg?s=612x612&w=0&k=20&c=Fn_R9Vo2vGbJv_qxLvCE37JbEWD7RJq0Gzb_dbl1abE=",
+    thumbnailUrl:
+      "https://media.istockphoto.com/id/1724789360/photo/digital-security-concept-and-cloud-computing-security.jpg?s=612x612&w=0&k=20&c=Fn_R9Vo2vGbJv_qxLvCE37JbEWD7RJq0Gzb_dbl1abE=",
     content: "This is card 3",
   },
   {
     id: "4",
     name: "Card 4",
-    logoUrl: "https://via.placeholder.com/150",
-    thumbnailUrl: "https://via.placeholder.com/600X400",
+    logoUrl:
+      "https://media.istockphoto.com/id/1820459753/vector/ai-chatbot-artificial-intelligence.jpg?s=612x612&w=0&k=20&c=iGsEpC8Tngwv64D89NS-NFFSS1NryHBrocdbkZo4sdg=",
+    thumbnailUrl:
+      "https://media.istockphoto.com/id/1820459753/vector/ai-chatbot-artificial-intelligence.jpg?s=612x612&w=0&k=20&c=iGsEpC8Tngwv64D89NS-NFFSS1NryHBrocdbkZo4sdg=",
     content: "This is card 4",
   },
 ];
--- lib/utilities/ImageCarouselData.ts
+++ lib/utilities/ImageCarouselData.ts
@@ -7,22 +7,26 @@ export interface CarouselImageItem {
 export const ImageCarouselData: CarouselImageItem[] = [
   {
     id: "1",
-    ImageUrl: "https://via.placeholder.com/1920x1080",
+    ImageUrl:
+      "https://media.istockphoto.com/id/1820459753/vector/ai-chatbot-artificial-intelligence.jpg?s=612x612&w=0&k=20&c=iGsEpC8Tngwv64D89NS-NFFSS1NryHBrocdbkZo4sdg=",
     redirectUrl: "https://www.google.com",
   },
   {
     id: "2",
-    ImageUrl: "https://via.placeholder.com/1920x1080",
+    ImageUrl:
+      "https://media.istockphoto.com/id/1724789360/photo/digital-security-concept-and-cloud-computing-security.jpg?s=612x612&w=0&k=20&c=Fn_R9Vo2vGbJv_qxLvCE37JbEWD7RJq0Gzb_dbl1abE=",
     redirectUrl: "https://www.google.com",
   },
   {
     id: "3",
-    ImageUrl: "https://via.placeholder.com/1920x1080",
+    ImageUrl:
+      "https://media.istockphoto.com/id/95442265/photo/lottery.jpg?s=2048x2048&w=is&k=20&c=ITM_3NwSm8S7D-XT1h-ak_Sah7jI0_9DxcTvweS84gk=",
     redirectUrl: "https://www.google.com",
   },
   {
     id: "4",
-    ImageUrl: "https://via.placeholder.com/1920x1080",
+    ImageUrl:
+      "https://media.istockphoto.com/id/1493264991/vector/financial-and-business.jpg?s=612x612&w=0&k=20&c=WIoXQQgz_J43DVtBN3Av_4EELjiMljQTivssv3uUOT0=",
     redirectUrl: "https://www.google.com",
   },
 ];
