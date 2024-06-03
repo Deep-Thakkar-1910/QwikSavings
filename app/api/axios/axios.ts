812c6129abbb2d44fca3c0cbbaff4a8b084d7a07
Deep-Thakkar-1910 <dthakkar477@gmail.com>
2024-06-03 17:16:18 +0530

    fix: base url for production

--- app/api/axios/axios.ts
+++ app/api/axios/axios.ts
@@ -1,6 +1,13 @@
 import axios from "axios";
 
 // Create an axios instance
+// For development
+// export default axios.create({
+//   baseURL: "http://localhost:3000/api",
+// });
+
+// Create an axios instance
+// For production
 export default axios.create({
-  baseURL: "http://localhost:3000/api",
+  baseURL: "https://qwikky-savings.vercel.app/api",
 });
