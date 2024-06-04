fa0bc1645426eae33fbb7e821aff69cc4bc6f162
Deep-Thakkar-1910 <dthakkar477@gmail.com>
2024-06-04 18:27:29 +0530

    fix: 0 index errors in route display route handlers

--- app/(Server)/api/(GET)/getdisplaystores/route.ts
+++ app/(Server)/api/(GET)/getdisplaystores/route.ts
@@ -1,11 +1,12 @@
 // Api handler to get categories with details
 
 import db from "@/lib/prisma";
+import { PrismaClientKnownRequestError } from "@prisma/client/runtime/library";
 import { NextResponse } from "next/server";
 
 export async function GET(req: Request) {
   const { searchParams } = new URL(req.url);
-  const page: number | undefined = Number(searchParams.get("page")) ?? 1;
+  const page: number = Number(searchParams.get("page")) || 1;
   const likeQuery: string | undefined = searchParams.get("like") ?? undefined;
   const limit = 20;
 
@@ -29,6 +30,14 @@ export async function GET(req: Request) {
     return NextResponse.json({ success: true, stores }, { status: 200 });
   } catch (err) {
     console.log(err);
+
+    if (err instanceof PrismaClientKnownRequestError) {
+      return NextResponse.json(
+        { success: false, error: err.message },
+        { status: 400 },
+      );
+    }
+
     return NextResponse.json(
       { success: false, error: "Internal server error" },
       { status: 500 },
--- app/api/axios/axios.ts
+++ app/api/axios/axios.ts
@@ -2,8 +2,17 @@ import axios from "axios";
 
 // Create an axios instance
 
+// For development
+// export default axios.create({
+//   baseURL: "http://localhost:3000/api",
+//   headers: {
+//     "Cache-Control": "no-cache",
+//   },
+// });
+
+// For production
 export default axios.create({
-  baseURL: `${process.env.BASE_URL}/api`,
+  baseURL: `https://qwikky-savings.vercel.app/api`,
   headers: {
     "Cache-Control": "no-cache",
     Pragma: "no-cache",
