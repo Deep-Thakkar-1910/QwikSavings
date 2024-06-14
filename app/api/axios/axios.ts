47497dd6265a0aa58900dcd7b825e927fdabee2c
Deep-Thakkar-1910 <dthakkar477@gmail.com>
2024-06-14 20:45:25 +0530

    fix: Changed base url for api requests

--- app/api/axios/axios.ts
+++ app/api/axios/axios.ts
@@ -3,15 +3,11 @@ import axios from "axios";
 // Create an axios instance
 
 // For development
-export default axios.create({
-  baseURL: "http://localhost:3000/api",
-});
+// export default axios.create({
+//   baseURL: "http://localhost:3000/api",
+// });
 
 // For production
-// export default axios.create({
-//   baseURL: `https://qwik-savings-eta.vercel.app/api`,
-//   headers: {
-//     "Cache-Control": "no-cache",
-//     Pragma: "no-cache",
-//   },
-// });
+export default axios.create({
+  baseURL: `https://qwik-savings-eta.vercel.app/api`,
+});
