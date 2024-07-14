99fc9263f4cba3535e38a9f28e1770c39a1fcd21
Deep Thakkar <dthakkar477@gmail.com>
2024-07-15 01:43:39 +0530

    fix: build time error for blog page

--- app/(Pages)/(Blogs)/blogs/page.tsx
+++ app/(Pages)/(Blogs)/blogs/page.tsx
@@ -1,12 +1,12 @@
 import db from "@/lib/prisma";
 import BlogDisplay from "../_BlogComponents/BlogsDisplay";
 
-export async function generateStaticParams() {
+/*export async function generateStaticParams() {
   const blogs = await db.blog.findMany({});
   return {
     paths: blogs.slice(0, 6).map((blog) => blog.blogId),
   };
-}
+}*/
 const EventPage = () => {
   return (
     <section className="mx-auto mb-10 flex w-full max-w-screen-xl flex-col  items-start px-4 sm:px-8 lg:px-16 xl:px-2 2xl:px-0">
