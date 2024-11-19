1ea4cda79989aa14c5fdbbdbbf54b7b96764630c
Sparshlohana <sparsh.lohana@gmail.com>
2024-11-19 21:42:32 +0530

    metadata added for store

--- app/(Pages)/(Store)/stores/[storename]/page.tsx
+++ app/(Pages)/(Store)/stores/[storename]/page.tsx
@@ -1,5 +1,26 @@
+import { Metadata } from "next";
 import DetailsPage from "@/app/(Pages)/_PageComponents/DetailsPage";
+
+interface Params {
+  params: {
+    storename: string;
+  };
+}
+
+export async function generateMetadata({ params }: Params): Promise<Metadata> {
+  const { storename } = params;
+
+  const title = `Store ${storename} - Explore Products`;
+  const description = `Discover products available in Store ${storename}. Shop now to explore unique items curated just for you.`;
+
+  return {
+    title,
+    description,
+  };
+}
+
 const StorePage = () => {
+
   return (
     <main className="overflow-x-hidden">
       <DetailsPage fetchFrom="store" />
