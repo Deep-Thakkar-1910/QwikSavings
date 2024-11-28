9bc86958997907ba0d940d3aadda1442f371f0cf
Deep-Thakkar-1910 <dthakkar477@gmail.com>
2024-11-29 00:38:57 +0530

    chore/Made database changes for configuring aws database

--- app/api/axios/axios.ts
+++ app/api/axios/axios.ts
@@ -3,15 +3,15 @@ import axios from "axios";
 // Create an axios instance
 
 // For development
-// export default axios.create({
-//   baseURL: "http://localhost:3000/api",
-// });
+export default axios.create({
+  baseURL: "http://localhost:3000/api",
+});
 
 // For production
 // export default axios.create({
 //   baseURL: `https://qwiksavings.com/api`,
 // });
 
-export default axios.create({
-  baseURL: `https://qwik-savings-eta.vercel.app//api`,
-});
+// export default axios.create({
+//   baseURL: `https://qwik-savings-eta.vercel.app/api`,
+// });
--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,12 +1,8 @@
 import { PrismaClient } from "@prisma/client";
-import { Pool } from "@neondatabase/serverless";
-import { PrismaNeon } from "@prisma/adapter-neon";
 
 // This function creates a new PrismaClient instance
 const prismaClientSingleton = () => {
-  const pool = new Pool({ connectionString: process.env.POSTGRES_PRISMA_URL });
-  const adapter = new PrismaNeon(pool);
-  return new PrismaClient({ adapter });
+  return new PrismaClient();
 };
 
 declare const globalThis: {
